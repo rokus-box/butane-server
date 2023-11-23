83425940466de1758d5b9cf9c4560bdc29340b01
Oguz Turkay <tyserract@gmail.com>
2023-11-23 19:40:28 +0300

    refactor: add ddb tf file

---
+++ terraform/ddb.tf
@@ -0,0 +1,21 @@
+resource "aws_dynamodb_table" "butane_table" {
+  name         = "ButaneTable"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "PK"
+  range_key    = "SK"
+
+  attribute {
+    name = "PK"
+    type = "S"
+  }
+
+  attribute {
+    name = "SK"
+    type = "S"
+  }
+
+  ttl {
+    attribute_name = "TTL"
+    enabled = true
+  }
+}
