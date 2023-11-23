c61330f9846932bc1f6645197fa9735ff6d88cdf
Oguz Turkay <tyserract@gmail.com>
2023-11-23 19:10:04 +0300

    refactor: removed deleting multiple sessions

--- node/sessions/app.mjs
+++ node/sessions/app.mjs
@@ -1,8 +1,6 @@
 'use strict';
 import { DynamoDB } from '@aws-sdk/client-dynamodb';
-import { json, status, text } from './util.mjs';
-import { authenticator } from 'otplib';
-
+import {  status } from './util.mjs';;
 
 const TABLE_NAME = process.env.TABLE_NAME;
 const dynamoDB = new DynamoDB({
@@ -15,84 +13,20 @@ export const handler = async (event) => {
     return status(401);
   }
 
-  const userId = await getUserId(event, dynamoDB, token);
+  const userId = await getUserId(dynamoDB, token);
 
   if ('' === userId) {
     return status(401);
   }
 
-  switch (event.httpMethod) {
-    case 'GET':
-      return handleGetSessions(event, userId, token);
-    case 'DELETE':
-      const mfaCode = event.headers['X-Mfa-Challenge'];
-      if (null == mfaCode) {
-        return text('X-Mfa-Challenge header is required', 401);
-      }
-
-      const isValid = authenticator.verify({
-        token: mfaCode,
-        // TODO: Kullanicinin MFA kodunu DynamoDB'den al U#<userId>
-        secret: 'JBSWY3DPEHPK3PXP',
-      });
-
-      console.log('isValid', isValid);
-
-      return text('OK');
-  }
+  return handleDeleteSession(userId, token);
 };
 
-async function handleGetSessions(event, userId, token) {
-  const res = await dynamoDB.query({
-    TableName: TABLE_NAME,
-    KeyConditionExpression: 'PK = :pk AND SK = :sk',
-    ProjectionExpression: 'ip_address, user_agent, timestamp',
-    ExpressionAttributeValues: {
-      ':pk': { S: 'SS#' + token },
-      ':sk': { S: userId },
-    },
-  }).catch((error) => {
-    console.error('error getting sessions', error);
-  });
-
-  return json(res.Items.map((item) => ({
-    ip_address: item.ip_address.S,
-    user_agent: item.user_agent.S,
-    timestamp: Number(item.timestamp.N),
-  })));
-}
-
-async function handleDeleteSessions(event, userId, token) {
-  const res = await dynamoDB.query({
-    TableName: TABLE_NAME,
-    KeyConditionExpression: 'PK = :pk AND begins_with(SK, :sk)',
-    ExpressionAttributeValues: {
-      ':pk': { S: 'SS#' + token },
-      ':sk': { S: userId },
-    },
-  }).catch((error) => {
-    console.error('error getting sessions', error);
-  });
-
-  const promises = res.Items.map((item) => {
-    return dynamoDB.deleteItem({
-      TableName: TABLE_NAME,
-      Key: {
-        PK: { S: item.PK.S },
-        SK: { S: item.SK.S },
-      },
-    });
-  });
-
-  await Promise.all(promises);
-
-  return status(204);
-}
-
-async function getUserId(event, dynamoDB, token) {
-  const res = await dynamoDB.query({
+async function getUserId(dynamoDB, token) {
+  const sessionsRes = await dynamoDB.query({
     TableName: TABLE_NAME,
     KeyConditionExpression: 'PK = :pk',
+    ProjectionExpression: 'SK',
     ExpressionAttributeValues: {
       ':pk': { S: 'SS#' + token },
     },
@@ -100,9 +34,23 @@ async function getUserId(event, dynamoDB, token) {
     console.error('error getting sessions', error);
   });
 
-  if (0 === res.Items.length) {
+  if (0 === sessionsRes.Items.length) {
     return '';
   }
 
-  return res.Items[0].SK.S;
+  return sessionsRes.Items[0].SK.S;
+}
+
+async function handleDeleteSession(userId, token) {
+  await dynamoDB.deleteItem({
+    TableName: TABLE_NAME,
+    Key: {
+      PK: { S: 'SS#' + token },
+      SK: { S: userId },
+    },
+  }).catch((error) => {
+    console.error('error deleting session', error);
+  });
+
+  return status(204);
 }
