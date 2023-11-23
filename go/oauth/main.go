b213b78b9d0aab20c48b782d5a2519afb7cf3ef3
Oguz Turkay <tyserract@gmail.com>
2023-11-23 19:07:30 +0300

    refactor: remove saving user agent and ip

--- go/oauth/main.go
+++ go/oauth/main.go
@@ -84,12 +84,9 @@ func saveUser(ctx context.Context, ddb *dynamodb.Client, u *c.User, passHash str
 // saveSession saves the session to DynamoDB and returns the token
 func saveSession(ctx context.Context, ddb *dynamodb.Client, s *c.Session) string {
 	item, _ := attributevalue.MarshalMap(c.MapA{
-		"PK":         "SS#" + s.Token,
-		"SK":         s.UserID,
-		"TTL":        s.TTL,
-		"user_agent": s.UserAgent,
-		"ip_address": s.IPAddress,
-		"timestamp":  s.Timestamp.Unix(),
+		"PK":  "SS#" + s.Token,
+		"SK":  s.UserID,
+		"TTL": s.TTL,
 	})
 
 	_, err := ddb.PutItem(ctx, &dynamodb.PutItemInput{
