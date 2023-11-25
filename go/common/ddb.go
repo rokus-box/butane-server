f67e1fb07f1f9714965bf5da57e0f618f005ba6f
Oguz Turkay <tyserract@gmail.com>
2023-11-25 20:05:02 +0300

    refactor: remove local ddb config

--- go/common/ddb.go
+++ go/common/ddb.go
@@ -12,19 +12,8 @@ import (
 
 var TableName = aws.String(os.Getenv("TABLE_NAME"))
 
-type resolver struct{}
-
-func (r resolver) ResolveEndpoint(service string, region string, options ...interface{}) (aws.Endpoint, error) {
-	return aws.Endpoint{
-		URL: "http://192.168.1.12:8000",
-	}, nil
-}
-
 func NewDDB() *dynamodb.Client {
-	cfg, err := config.LoadDefaultConfig(context.TODO(),
-		config.WithRegion("eu-central-1"),
-		config.WithEndpointResolverWithOptions(resolver{}),
-	)
+	cfg, err := config.LoadDefaultConfig(context.TODO(), config.WithRegion("eu-central-1"))
 
 	if nil != err {
 		panic(err)
